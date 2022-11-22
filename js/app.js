d00653cc9b64c8987986a97c76424c5ce27fdc43
Sherita Manning <smanningsn@gmail.com>
2022-11-21 23:51:55 -0800

    Updated html, css and javascript files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Mobile Hamburger Menu Animation
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
@@ -9,3 +10,17 @@ menuBtn.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+// Content Scroll In Animations
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
